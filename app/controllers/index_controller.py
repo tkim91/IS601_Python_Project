f0ff22ffed0e314f67b20fb3f492737a30ceeb3d
Tom <thomasdhkim91@gmail.com>
2022-02-07 18:37:41 -0500

    Updated Name

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -6,5 +6,5 @@ from  . controller import ControllerBase
 class IndexController(ControllerBase):
     @staticmethod
     def get():
-        name = "kwilliam"
+        name = "Thomas Kim"
         return render_template('index.html', name=name)
